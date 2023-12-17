00c37c594d362908bd4040b7c373aa0e22bfc8d1
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-17 11:36:53 -0500

    feat: add fonts, lightswitch/theme toggle

---
+++ public/scripts/lightswitch.js
@@ -0,0 +1,29 @@
+const systemTheme = () =>
+	window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+function setTheme(theme, permanent = false) {
+	document.documentElement.setAttribute("theme", theme);
+	if (permanent) localStorage.setItem("theme", theme);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	setTheme(localStorage.getItem("theme") || systemTheme());
+
+	document.querySelector("#lightswitch").addEventListener("click", () => {
+		setTheme(
+			document.documentElement.getAttribute("theme") === "light"
+				? "dark"
+				: "light",
+			true
+		);
+	});
+
+	window
+		.matchMedia("(prefers-color-scheme: dark)")
+		.addEventListener("change", (event) => {
+			if (!localStorage.getItem("theme")) {
+				setTheme(event.matches ? "dark" : "light");
+			}
+		});
+});
+s;
